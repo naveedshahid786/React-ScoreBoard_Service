94207364258075f1cdd9ff52d2f9821eaaaaa2a9
Naveed Shahid <naveedshahid91@gmail.com>
2020-02-29 20:28:01 +0200

    Players List Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
 import './App.css';
 
 function Header() {
+    
   return (
     <div className="header">
       
@@ -12,15 +12,44 @@ function Header() {
   );
 }
 
+function Player(props) {
+  return (
+    
+      <div className="player-name">
+        
+        {props.name}
+      </div>         
+  );
+}
+
 class App extends Component {
-  
+
+    constructor(props) {
+    super(props);
+    this.state = {
+      players: this.props.initialPlayers,  
+    }
+  }
+
   render() {
     return (
       <div className="scoreboard">
-        <Header title={this.header}  />          
-                         
-        </div>
-    
+        <Header title={this.header} 
+         players={this.state.players}/>
+        
+        <div className="players">
+          {this.state.players.map((player, index) => {
+        
+            return (
+                
+              <Player
+                name={player.name}
+                score={player.score}
+                key={player.id} />
+            )
+          })}         
+        </div>      
+        </div>    
     );
   }
 }
