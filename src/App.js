d9cdc4c9f203f00a9e82412f4ac761c5bf2323c2
Naveed Shahid <naveedshahid91@gmail.com>
2020-02-29 21:34:13 +0200

    Remove Players Function Added

--- src/App.js
+++ src/App.js
@@ -15,22 +15,50 @@ function Header() {
 function Player(props) {
   return (
     
+<div className="player">
       <div className="player-name">
-        
+        <a className="remove-player" onClick={props.onRemove}> &times; </a>
         {props.name}
-      </div>         
+      </div>
+                
+      <div className="player-score">
+        <Counter score={props.score} onChange={props.onScoreChange} />          
+      </div>
+    </div>
+  );
+}
+
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement" onClick={() => props.onChange(-1)}> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment" onClick={() => props.onChange(1)}> + </button>
+    </div>
   );
 }
 
 class App extends Component {
-
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      players: this.props.initialPlayers,  
+      players: this.props.initialPlayers,
+      
     }
+    
+  }
+  onScoreChange(index, delta) {
+    let players = this.state.players.slice();
+    players[index].score += delta;
+    this.setState({players: players});
   }
 
+  onPlayerRemove(index) {
+    let players = this.state.players.slice();
+    players.splice(index, 1);
+    this.setState({players: players});
+  }
+    
   render() {
     return (
       <div className="scoreboard">
@@ -45,7 +73,9 @@ class App extends Component {
               <Player
                 name={player.name}
                 score={player.score}
-                key={player.id} />
+                key={player.id}
+                onScoreChange={(delta) => {this.onScoreChange(index, delta)}}
+                onRemove={() => {this.onPlayerRemove(index)}} />
             )
           })}         
         </div>      
